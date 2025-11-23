0ed3cabae00dba09476f19f1e0253ca363069a2b
Ash <157792270+ash2213@users.noreply.github.com>
2025-11-23 16:43:05 +0100

    Add files via upload

---
+++ gnomesort/gnomesort.js
@@ -0,0 +1,12 @@
+export function gnomeSort(arr) {
+    let index = 1;
+    while (index < arr.length) {
+        if (index === 0 || arr[index] >= arr[index - 1]) {
+            index++;
+        } else {
+            [arr[index], arr[index - 1]] = [arr[index - 1], arr[index]];
+            index--;
+        }
+    }
+    return arr;
+}
---
+++ gnomesort/gnomesortTest.js
@@ -0,0 +1,4 @@
+import { gnomeSort } from "./gnomesort.js";
+
+const list = [34, 2, 23, 67, 4, 18, 99, 1];
+console.log(gnomeSort(list));
